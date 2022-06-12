f4fcb3a79408fdb2b3ba076d982b551299f1be9d
expelledboy Anthony Jackson <102334+expelledboy@users.noreply.github.com>
2022-06-12 14:24:02 +0200

    init firebase emulators

--- shell.nix
+++ shell.nix
@@ -2,7 +2,21 @@ with (import <nixpkgs> {});
 
 mkShell {
   buildInputs = [
+    which
+    jdk
     nodejs
     nodePackages.yarn
   ];
+
+  NPM_CONFIG_PREFIX = builtins.toString ./.npm-packages;
+
+  shellHook = ''
+    test -d node_modules || yarn install
+  '';
+
+  postHook = ''
+    export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
+    which expo || yarn global add expo-cli
+    which firebase || yarn global add firebase-tools
+  '';
 }
